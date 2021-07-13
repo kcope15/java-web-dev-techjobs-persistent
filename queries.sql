6ec7b1c6e36eadf34d820be0966e2353bb89d56c
Katie <cope_katie@yahoo.com>
2021-07-12 20:06:03 -0500

    finished

--- queries.sql
+++ queries.sql
@@ -1,7 +1,13 @@
 ## Part 1: Test it with SQL
+--id int
+--employer varchar
+--name varchar
+--skills varchar
 
 ## Part 2: Test it with SQL
+--select name from employer where (location = "Saint Louis City")
 
 ## Part 3: Test it with SQL
+--DROP TABLE `techjobs`.`job`;
 
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,38 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 250)
+    private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Skill() { }
+
+    public Skill(String description) {
+        super();
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
 }
